0b970b1238347495c05d04ea8a4ef25631b1351d
Ataru Kodaka <ataru.kodaka@gmail.com>
2015-12-07 13:03:24 +0000

    competition_watcher

---
+++ competition_watcher.rb
@@ -0,0 +1,82 @@
+require 'active_record'
+require 'yaml'
+require 'pg'
+require 'pry-byebug'
+require 'csv'
+require 'logger'
+
+require './model.rb'
+require './competition_parser.rb'
+
+module CompetitionWatcher
+  class Database
+    def initialize
+      @log = Logger.new(STDERR)
+      connect_database
+    end
+    def connect_database
+      @log.info("connection to database...")
+      local_db_address = "postgresql://postgres@192.168.33.10/competition_db"
+      db_address = ENV['HEROKU_POSTGRESQL_TEAL_URL'] || local_db_address
+      ActiveRecord::Base.establish_connection(db_address)
+    end
+
+    def update
+      # read input table
+      competition_csv_filename = "competitions.csv"
+      @log.info(" read #{competition_csv_filename}...")
+      tbl = CSV::table(competition_csv_filename)
+      headers = tbl.headers
+
+      tbl.each {|c|   ## for each competitions
+        next if c[:status] == "skip"
+        ## database
+        competition = Competition.find_by_key(c[:key])
+        competition = Competition.create if competition.nil?
+        headers.each {|h| competition[h] = c[h] }
+        competition.save
+
+        @log.info("#{c[:name]},  #{c[:site_url]}")
+        
+        site_url = c[:site_url]
+        parser = Fisk8::CompetitionParser.new(site_url)
+        parser.offset_timezone = c[:timezone]
+        summary = parser.parse_summary
+        
+        ## entry
+        summary.each {|category, value|
+          @log.info(" entry for #{category} on #{value[:entry_url]}")
+
+          ## clear entries if exists to avoid duplication
+          competition.entries.where(category: category).each {|e|
+            e.destroy
+          }
+          
+          hash = parser.parse_category_entries(value[:entry_url])
+          hash.each {|k, v|
+            entry = competition.entries.create(category: category, number: v[:number])
+            skater_name = v[:name]
+            skater_nation = v[:nation]
+            
+            if skater = Skater.find_by_name(skater_name)
+            else
+              skater = Skater.create(name: skater_name, nation: skater_nation, category: category)
+              skater.save
+            end
+            entry.skater_id = skater.id
+            entry.save
+          }
+        }
+      }
+      
+    end
+    def update_skaters
+    end
+  end  ## class
+end
+
+if $0 == __FILE__
+  watcher = CompetitionWatcher::Database.new
+  watcher.update
+  watcher.update_skaters
+end
